2188583bdaf955d4beeccb71b40012e7565d8a13
Ali <alidoran@gmail.com>
2020-11-27 23:09:20 +0330

    -- ADD INSTANCE FOR AppDatabase -- REMOVE THREAD

--- AppDatabase.java
+++ AppDatabase.java
@@ -1,9 +1,21 @@
 package com.example.room_learning;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {User.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase appDatabase;
+
+    public static AppDatabase getInstance(Context context) {
+        if (appDatabase == null)
+            appDatabase = Room.databaseBuilder(context,AppDatabase.class, "room_database").allowMainThreadQueries().build();
+        return appDatabase;
+    }
+
     public abstract UserDao userDao();
 }
--- MainActivity.java
+++ MainActivity.java
@@ -17,34 +17,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        
-        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
-                AppDatabase.class, "UserDataBase")
-                .build();
-
         Button btnSaveMain = findViewById(R.id.main_save_btn);
         Button btnShowMain = findViewById(R.id.main_show_btn);
 
-        btnSaveMain.setOnClickListener(v->{
-
-            new Thread(() -> {
-                User user = new User();
-                user.uid = 1;
-                user.firstName = "ali";
-                user.lastName = "doran";
-                db.userDao().insertAll(user);
-            }).start();
-
+        btnSaveMain.setOnClickListener(v -> {
+            User user = new User();
+            user.uid = 2;
+            user.firstName = "ali";
+            user.lastName = "doran";
+            AppDatabase.getInstance(this).userDao().insertAll(user);
         });
 
-        btnShowMain.setOnClickListener(v->{
-
-            new Thread(() -> {
-                List<User> userList = db.userDao().getAll();
-                runOnUiThread(() -> Toast.makeText(MainActivity.this, userList.get(0).firstName
-                        + userList.get(0).lastName
-                        + userList.get(0).uid, Toast.LENGTH_SHORT).show());
-            }).start();
-        });
+            btnShowMain.setOnClickListener(v -> {
+                List<User> userList =AppDatabase.getInstance(this).userDao().getAll();
+                     Toast.makeText(MainActivity.this, userList.get(0).firstName
+                            + userList.get(0).lastName
+                            + userList.get(0).uid, Toast.LENGTH_SHORT);
+            });
+        }
     }
-}
